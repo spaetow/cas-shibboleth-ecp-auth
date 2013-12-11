069853cd91021e9743cf6df48a05a80a5a2352a6
Stefan Paetow <stefan.paetow@diamond.ac.uk>
2013-12-11 03:55:53 +0000

    Cleaned up code structure (cosmetics only).

--- shibboleth-ecp-authenticator4/src/main/java/uk/ac/diamond/cas/shibboleth/authentication/handler/ShibbolethAuthenticationHandler.java
+++ shibboleth-ecp-authenticator4/src/main/java/uk/ac/diamond/cas/shibboleth/authentication/handler/ShibbolethAuthenticationHandler.java
@@ -57,182 +57,182 @@ import uk.ac.diamond.shibbolethecpauthclient.ShibbolethECPAuthClient;
  */
 public class ShibbolethAuthenticationHandler extends AbstractUsernamePasswordAuthenticationHandler {
 
-    /** 
-     * Identifies the Shibboleth IdP to try and authenticate against. 
-     */
-    @NotNull
-    private String IdP;
+	/** 
+	 * Identifies the Shibboleth IdP to try and authenticate against. 
+	 */
+	@NotNull
+	private String IdP;
 
-    /**
-     * Identifies the Shibboleth SP we want to run ECP against. 
-     */
-    @NotNull
-    private String SP;
+	/**
+	 * Identifies the Shibboleth SP we want to run ECP against. 
+	 */
+	@NotNull
+	private String SP;
 
-    /**
-     * Identifies the eventual principal in the SAML response.
-     */
-    private String attribute;
+	/**
+	 * Identifies the eventual principal in the SAML response.
+	 */
+	private String attribute;
 
-    /**
-     * Identifies an optional proxy host to connect to the IdP and the SP with.
-     */
-    private String proxyHost;
+	/**
+	 * Identifies an optional proxy host to connect to the IdP and the SP with.
+	 */
+	private String proxyHost;
 
-    /**
-     * Identifies an optional proxy port to use with the proxy host.
-     */
-    @Min(0)
-    private int proxyPort;
+	/**
+	 * Identifies an optional proxy port to use with the proxy host.
+	 */
+	@Min(0)
+	private int proxyPort;
 
-    /**
-     * Disables certificate checking, usually used with self-signed certificates. 
-     */
-    @NotNull
-    private boolean disableCertCheck;
+	/**
+	 * Disables certificate checking, usually used with self-signed certificates. 
+	 */
+	@NotNull
+	private boolean disableCertCheck;
 
-    @Override
-    protected final Principal authenticateUsernamePasswordInternal(final String username, final String password)
-            throws GeneralSecurityException, PreventedException {
+	@Override
+	protected final Principal authenticateUsernamePasswordInternal(final String username, final String password)
+			throws GeneralSecurityException, PreventedException {
 
-        logger.debug("Attempting to authenticate {} at {}", username, IdP);
+		logger.debug("Attempting to authenticate {} at {}", username, IdP);
 
-        try {
-            // Initialise the library
-            DefaultBootstrap.bootstrap();
-            final BasicParserPool parserPool = new BasicParserPool();
-            parserPool.setNamespaceAware(true);
+		try {
+			// Initialise the library
+			DefaultBootstrap.bootstrap();
+			final BasicParserPool parserPool = new BasicParserPool();
+			parserPool.setNamespaceAware(true);
 
-            // Set proxy
-            HttpHost proxy = null;
-            logger.debug("Setting proxy");
-            if ((this.proxyHost != null) && (!this.proxyHost.isEmpty())) {
-                if (this.proxyPort == 0) {
-                    proxy = new HttpHost(this.proxyHost, 8080);
-                } else {
-                	proxy = new HttpHost(this.proxyHost, this.proxyPort);
-                }
-            }
-            logger.debug("Set proxy successfully");
-            
-            // Instantiate a copy of the client, try to authentication, catch any errors that occur
-            ShibbolethECPAuthClient ecpClient = new ShibbolethECPAuthClient(proxy, this.IdP, 
-            		this.SP, disableCertCheck);
-        	Response response = ecpClient.authenticate(username, password);
-            logger.debug("Successfully authenticated {}", username);
+			// Set proxy
+			HttpHost proxy = null;
+			logger.debug("Setting proxy");
+			if ((this.proxyHost != null) && (!this.proxyHost.isEmpty())) {
+				if (this.proxyPort == 0) {
+					proxy = new HttpHost(this.proxyHost, 8080);
+				} else {
+					proxy = new HttpHost(this.proxyHost, this.proxyPort);
+				}
+			}
+			logger.debug("Set proxy successfully");
 
-            // if the attribute is empty, we simply authenticate and return the username as principal
-            if ((this.attribute == null) || (this.attribute.isEmpty()))
-            {
-            	return new SimplePrincipal(username);
-            }
+			// Instantiate a copy of the client, try to authentication, catch any errors that occur
+			ShibbolethECPAuthClient ecpClient = new ShibbolethECPAuthClient(proxy, this.IdP, 
+					this.SP, disableCertCheck);
+			Response response = ecpClient.authenticate(username, password);
+			logger.debug("Successfully authenticated {}", username);
 
-            // get the first assertion in the response. Any exceptions here are a problem
-            List<Attribute> attributes = response.getAssertions().get(0)
-            		// get the first (and should be only) attribute statement
-                    .getAttributeStatements().get(0)
-                    // get all attributes
-                    .getAttributes();
+			// if the attribute is empty, we simply authenticate and return the username as principal
+			if ((this.attribute == null) || (this.attribute.isEmpty()))
+			{
+				return new SimplePrincipal(username);
+			}
 
-            // if there are no attributes, we can't do a lookup.
-            if (attributes.isEmpty()) {
-                throw new AttributeNotFoundException("The Shibboleth Identity Provider at " +
-                		this.IdP + " returned a SAML assertion with no attributes");
-            }
+			// get the first assertion in the response. Any exceptions here are a problem
+			List<Attribute> attributes = response.getAssertions().get(0)
+					// get the first (and should be only) attribute statement
+					.getAttributeStatements().get(0)
+					// get all attributes
+					.getAttributes();
 
-            // trawl the attributes to check if we can find ours
-            String lookupAttributeValue = null;
-            boolean idFound = false;
-            for (Attribute attribute : attributes) {
-                if ((attribute.getName().equals(this.attribute)) ||
-                    (attribute.getFriendlyName().equals(this.attribute))) {
-                    idFound = true;
-                    XMLObject attributeValue = attribute.getAttributeValues().get(0);
-                    if (attributeValue instanceof XSString) {
-                        lookupAttributeValue = ((XSString) attributeValue).getValue();
-                    } else if (attributeValue instanceof XSAny) {
-                        lookupAttributeValue = ((XSAny) attributeValue).getTextContent();
-                    }
-                    logger.debug("Attribute: " + this.attribute + ", value: " + lookupAttributeValue);
-                    break;
-                } // if getName()...
-            } // for attribute...
+			// if there are no attributes, we can't do a lookup.
+			if (attributes.isEmpty()) {
+				throw new AttributeNotFoundException("The Shibboleth Identity Provider at " +
+						this.IdP + " returned a SAML assertion with no attributes");
+			}
 
-            // Attribute was not found in the SAML statement
-            if (!idFound) {
-                throw new AttributeNotFoundException("The attribute " + 
-                        this.attribute + " was not returned by the Shibboleth Identity Provider.");
-            }
-            
-            logger.info("Authentication was successful. Credential {} mapped to {}", username, lookupAttributeValue);
-            return new SimplePrincipal(lookupAttributeValue);
-            
-        } catch (final AttributeNotFoundException e) {
-            logger.debug("AttributeNotFoundException raised: {}", e.toString());
-            throw new FailedLoginException(e.toString());
-        } catch (final AuthenticationException e) {
-            logger.debug("AuthenticationException raised: {}", e.toString());
-            throw new FailedLoginException(e.toString());
-        } catch (final IOException e) {
-            logger.debug("IOException raised: {}", e.toString());
-            throw new PreventedException(e);
-        } catch (final Exception e) {
-            logger.debug("Exception raised: {}", e.toString());
-            throw new PreventedException(e);
-        }
-    }
+			// trawl the attributes to check if we can find ours
+			String lookupAttributeValue = null;
+			boolean idFound = false;
+			for (Attribute attribute : attributes) {
+				if ((attribute.getName().equals(this.attribute)) ||
+						(attribute.getFriendlyName().equals(this.attribute))) {
+					idFound = true;
+					XMLObject attributeValue = attribute.getAttributeValues().get(0);
+					if (attributeValue instanceof XSString) {
+						lookupAttributeValue = ((XSString) attributeValue).getValue();
+					} else if (attributeValue instanceof XSAny) {
+						lookupAttributeValue = ((XSAny) attributeValue).getTextContent();
+					}
+					logger.debug("Attribute: " + this.attribute + ", value: " + lookupAttributeValue);
+					break;
+				} // if getName()...
+			} // for attribute...
 
-    /**
-     * Identifies the eventual principal in the SAML response.
-     * 
-     * @param attribute string identifying the eventual principal in the SAML response.
-     */
-    public void setAttribute(final String attribute) {
-        this.attribute = attribute;
-    }
+			// Attribute was not found in the SAML statement
+			if (!idFound) {
+				throw new AttributeNotFoundException("The attribute " + 
+						this.attribute + " was not returned by the Shibboleth Identity Provider.");
+			}
 
-    /**
-     * Identifies the Shibboleth IdP to try and authenticate against.
-     * 
-     * @param IdP string specifying the full IdP ECP URL to authenticate against.
-     */
-    public void setIdP(final String IdP) {
-        this.IdP = IdP;
-    }
+			logger.info("Authentication was successful. Credential {} mapped to {}", username, lookupAttributeValue);
+			return new SimplePrincipal(lookupAttributeValue);
 
-    /**
-     * Identifies the Shibboleth SP we want to run ECP against.
-     * 
-     * @param SP string specifying a Shibboleth-protected URL to initiate the authentication with. 
-     */
-    public void setSP(final String SP) {
-        this.SP = SP;
-    }
+		} catch (final AttributeNotFoundException e) {
+			logger.debug("AttributeNotFoundException raised: {}", e.toString());
+			throw new FailedLoginException(e.toString());
+		} catch (final AuthenticationException e) {
+			logger.debug("AuthenticationException raised: {}", e.toString());
+			throw new FailedLoginException(e.toString());
+		} catch (final IOException e) {
+			logger.debug("IOException raised: {}", e.toString());
+			throw new PreventedException(e);
+		} catch (final Exception e) {
+			logger.debug("Exception raised: {}", e.toString());
+			throw new PreventedException(e);
+		}
+	}
 
-    /**
-     * Identifies an optional proxy host to connect to the IdP and the SP with.
-     * 
-     * @param proxyHost string specifying the proxy host to connect with.
-     */
-    public void setProxyHost(final String proxyHost) {
-        this.proxyHost = proxyHost;
-    }
+	/**
+	 * Identifies the eventual principal in the SAML response.
+	 * 
+	 * @param attribute string identifying the eventual principal in the SAML response.
+	 */
+	public void setAttribute(final String attribute) {
+		this.attribute = attribute;
+	}
 
-    /**
-     * Identifies an optional proxy port to use with the proxy host.
-     * 
-     * @param proxyPort integer specifying the proxy port to connect with. 
-     */
-    public void setProxyPort(final int proxyPort) {
-        this.proxyPort = proxyPort;
-    }
+	/**
+	 * Identifies the Shibboleth IdP to try and authenticate against.
+	 * 
+	 * @param IdP string specifying the full IdP ECP URL to authenticate against.
+	 */
+	public void setIdP(final String IdP) {
+		this.IdP = IdP;
+	}
 
-    /**
-     * Disables certificate checking, usually used with self-signed certificates.
-     * 
-     * @param disableCertCheck boolean specifying whether to disable certificate checking. 
-     */
-    public void setDisableCertCheck(final boolean disableCertCheck) {
-        this.disableCertCheck = disableCertCheck;
-    }
+	/**
+	 * Identifies the Shibboleth SP we want to run ECP against.
+	 * 
+	 * @param SP string specifying a Shibboleth-protected URL to initiate the authentication with. 
+	 */
+	public void setSP(final String SP) {
+		this.SP = SP;
+	}
+
+	/**
+	 * Identifies an optional proxy host to connect to the IdP and the SP with.
+	 * 
+	 * @param proxyHost string specifying the proxy host to connect with.
+	 */
+	public void setProxyHost(final String proxyHost) {
+		this.proxyHost = proxyHost;
+	}
+
+	/**
+	 * Identifies an optional proxy port to use with the proxy host.
+	 * 
+	 * @param proxyPort integer specifying the proxy port to connect with. 
+	 */
+	public void setProxyPort(final int proxyPort) {
+		this.proxyPort = proxyPort;
+	}
+
+	/**
+	 * Disables certificate checking, usually used with self-signed certificates.
+	 * 
+	 * @param disableCertCheck boolean specifying whether to disable certificate checking. 
+	 */
+	public void setDisableCertCheck(final boolean disableCertCheck) {
+		this.disableCertCheck = disableCertCheck;
+	}
 }
